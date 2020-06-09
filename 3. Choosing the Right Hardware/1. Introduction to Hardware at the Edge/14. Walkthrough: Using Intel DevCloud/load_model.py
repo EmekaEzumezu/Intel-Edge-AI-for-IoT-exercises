b567f835fd39ec5ecd52ec7b8bce064be1f151a2
Chukwuemeka Ezumezu <emyfrancis2012@gmail.com>
2020-06-09 19:28:11 +0100

    Complete practicing Walkthrough Using Intel DevCloud

---
+++ 3. Choosing the Right Hardware/1. Introduction to Hardware at the Edge/14. Walkthrough: Using Intel DevCloud/load_model.py
@@ -0,0 +1,25 @@
+
+import time
+from openvino.inference_engine import IENetwork
+from openvino.inference_engine import IECore
+import argparse
+
+def main(args):
+    model=args.model_path
+    model_weights=model+'.bin'
+    model_structure=model+'.xml'
+    
+    start=time.time()
+    model=IENetwork(model_structure, model_weights)
+
+    core = IECore()
+    net = core.load_network(network=model, device_name=device, num_requests=1)
+
+    print(f"Time taken to load model = {time.time()-start} seconds")
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--model_path', required=True)
+    
+    args=parser.parse_args() 
+    main(args)
